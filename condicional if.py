31e76735549dfb255804af302196bcb558837946
carlosdelima00 <114445774+carlosdelima00@users.noreply.github.com>
2022-09-29 10:14:28 -0300

    Add files via upload

---
+++ condicional if.py
@@ -0,0 +1,24 @@
+# uso do condicional "if"
+#ex:
+#"if" usado em valores booleanos!
+valor=True
+if valor:
+     print("oi")
+
+print("fim")
+
+a=20
+b=80
+res=0
+op="+"
+if op=="+":
+    res=a+b
+    print(str(a) + op + str(b) + " = " + str(res))
+print("parabens")   
+a=20
+b=80
+res=0
+op="-"
+if op=="-":
+    res=(b-a) * 2
+    print(str(b) + op + str(a) + " x "  + " 2 " + " = " + str(res))
